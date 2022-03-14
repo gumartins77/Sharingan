02b065c0e8857ca9fc3e6564e8ca46a3e72aacf3
gumartins77 <gugumartin77@gmail.com>
2022-03-14 10:50:53 -0300

    projeto01-m02

---
+++ assets/js/index.js
@@ -0,0 +1,32 @@
+let detalhe = document.querySelector('#detalhe'); 
+let som = document.querySelector('#som')
+let imagem = document.querySelector('#imagem');
+let botao = document.querySelector('#alteracao');
+botao.addEventListener('click', () =>{
+    if(botao.value == '1'){
+        detalhe.innerText = '2 Tomoes: Segundo nível'
+        som.play(); 
+        imagem.src = './assets/img/Sharingan-2.jpeg'
+        botao.value = '2' 
+    } else if(botao.value == '2') { 
+        detalhe.innerText = '3 Tomoes: Terceiro nível' 
+        som.play();
+        imagem.src = './assets/img/Sharingan-3.jpg'
+        botao.value = '3'  
+    } else if(botao.value == '3') { 
+        detalhe.innerText = 'Mangekyo Sharingan: Quarto nível' 
+        som.play();
+        imagem.src = './assets/img/Sharingan-4.jpg' 
+        botao.value = '4' 
+    } else if(botao.value == '4') { 
+        detalhe.innerText = 'Mangekyo Sharingan Eterno: Quinto nível' 
+        som.play();
+        imagem.src = './assets/img/Sharingan-5.webp' 
+        botao.value = '5' 
+    } else { 
+        detalhe.innerText = '1 Tomoe: Primeiro nível' 
+        som.play();
+        imagem.src = './assets/img/Sharingan-1.jpg' 
+        botao.value = '1' 
+    }
+});
